a5cab572156bd5130c5ae4c46f0ce54e51b36be1
ANTOND <71350868+antond15@users.noreply.github.com>
2022-06-02 16:42:48 +0200

    refactor: Update to client rendering API

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import './index.scss';
 import App from './components/App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './theme';
 
-render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
