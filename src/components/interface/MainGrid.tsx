ac8b82100627c9893bd26dfae6f7c179af792865
ANTOND <71350868+antond15@users.noreply.github.com>
2022-06-02 17:10:42 +0200

    refactor(grid): Separate into individual components

--- src/components/interface/MainGrid.tsx
+++ src/components/interface/MainGrid.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
-import {
-  SimpleGrid,
-  Image,
-  Flex,
-  Box,
-  Stack,
-  Popover,
-  PopoverBody,
-  PopoverContent,
-  PopoverTrigger,
-  PopoverArrow,
-  PopoverHeader,
-} from '@chakra-ui/react';
+import { SimpleGrid, Flex, Box } from '@chakra-ui/react';
+import ItemImage from '../item/ItemImage';
 import { items } from '../../data/items';
 
 const MainGrid: React.FC = () => {
@@ -21,23 +10,7 @@ const MainGrid: React.FC = () => {
         <Flex justifyContent="center">
           <SimpleGrid columns={5} spacingX="5px" spacingY="5px" w="fit-content">
             {items.map((data) => (
-              <Stack w="100px" h="125px" background="gray.700" borderColor="gray.600" borderRadius={5} overflow="hidden" spacing={0}>
-                <Image src={data.source} alt={data.label} w="100px" h="100px" objectFit="contain" />
-                <Popover trigger="hover">
-                  <PopoverTrigger>
-                    <Box h="25px" color="gray.300" background="gray.600">
-                      {data.label}
-                    </Box>
-                  </PopoverTrigger>
-                  <PopoverContent textAlign="left">
-                    <PopoverArrow />
-                    <PopoverHeader fontWeight="semibold">{data.label}</PopoverHeader>
-                    <PopoverBody>
-                      <Box>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, culpa.</Box>
-                    </PopoverBody>
-                  </PopoverContent>
-                </Popover>
-              </Stack>
+              <ItemImage {...data} />
             ))}
           </SimpleGrid>
         </Flex>
---
+++ src/components/item/ItemImage.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Stack, Image, Box, Popover, PopoverArrow, PopoverBody, PopoverContent, PopoverHeader, PopoverTrigger } from '@chakra-ui/react';
+
+type ItemProps = {
+  label: string;
+  source: string;
+  tags: string[];
+};
+
+const ItemImage: React.FC<ItemProps> = (props) => {
+  return (
+    <Stack w="100px" h="125px" background="gray.700" borderColor="gray.600" borderRadius={5} overflow="hidden" spacing={0}>
+      <Image src={props.source} alt={props.label} w="100px" h="100px" objectFit="contain" />
+      <Popover trigger="hover">
+        <PopoverTrigger>
+          <Box h="25px" color="gray.300" background="gray.600">
+            {props.label}
+          </Box>
+        </PopoverTrigger>
+        <PopoverContent textAlign="left">
+          <PopoverArrow />
+          <PopoverHeader fontWeight="semibold">{props.label}</PopoverHeader>
+          <PopoverBody>
+            <Box>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, culpa.</Box>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    </Stack>
+  );
+};
+
+export default ItemImage;
