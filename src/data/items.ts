9a9a9b94ad38510ab3ec529993b318937de7923c
ANTOND <71350868+antond15@users.noreply.github.com>
2022-06-02 16:43:05 +0200

    feat(items): More example items

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,5 +1,35 @@
 export const items = [
   {
+    label: 'Big slice of onion',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305103769610/onion.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Paperbag',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305393160222/paperbag.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Ammo 9mm',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305703567421/ammo-9.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Among Us',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305959411803/amogus.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Armor',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306185916446/armor.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Bandage',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306437558322/bandage.png',
+    tags: ['food', 'fruit'],
+  },
+  {
     label: 'Onion',
     source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305103769610/onion.png',
     tags: ['food', 'fruit'],
@@ -27,6 +57,231 @@ export const items = [
   {
     label: 'Bandage',
     source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306437558322/bandage.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Onion',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305103769610/onion.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Paperbag',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305393160222/paperbag.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Ammo 9mm',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305703567421/ammo-9.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Among Us',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305959411803/amogus.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Armor',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306185916446/armor.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Bandage',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306437558322/bandage.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Onion',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305103769610/onion.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Paperbag',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305393160222/paperbag.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Ammo 9mm',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305703567421/ammo-9.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Among Us',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305959411803/amogus.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Armor',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306185916446/armor.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Bandage',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306437558322/bandage.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Onion',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305103769610/onion.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Paperbag',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305393160222/paperbag.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Ammo 9mm',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305703567421/ammo-9.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Among Us',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305959411803/amogus.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Armor',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306185916446/armor.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Bandage',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306437558322/bandage.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Onion',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305103769610/onion.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Paperbag',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305393160222/paperbag.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Ammo 9mm',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305703567421/ammo-9.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Among Us',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305959411803/amogus.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Armor',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306185916446/armor.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Bandage',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306437558322/bandage.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Onion',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305103769610/onion.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Paperbag',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305393160222/paperbag.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Ammo 9mm',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305703567421/ammo-9.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Among Us',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305959411803/amogus.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Armor',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306185916446/armor.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Bandage',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306437558322/bandage.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Onion',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305103769610/onion.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Paperbag',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305393160222/paperbag.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Ammo 9mm',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305703567421/ammo-9.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Among Us',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305959411803/amogus.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Armor',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306185916446/armor.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Bandage',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306437558322/bandage.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Onion',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305103769610/onion.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Paperbag',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305393160222/paperbag.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Ammo 9mm',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305703567421/ammo-9.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Among Us',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651305959411803/amogus.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Armor',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306185916446/armor.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Bandage',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306437558322/bandage.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Google',
+    source: 'https://www.google.cz/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Money',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306768896000/money.png',
+    tags: ['food', 'fruit'],
+  },
+  {
+    label: 'Money',
+    source: 'https://cdn.discordapp.com/attachments/751544501377695764/981651306768896000/money.png',
     tags: ['food', 'fruit'],
   },
   {
