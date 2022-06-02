ba21ba66c54f55c9d0c2e656c9d5b5c81eebead8
ANTOND <71350868+antond15@users.noreply.github.com>
2022-06-02 16:44:38 +0200

    feat(grid): Item labels

--- src/components/interface/MainGrid.tsx
+++ src/components/interface/MainGrid.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SimpleGrid, Image, Flex, Box } from '@chakra-ui/react';
+import { SimpleGrid, Image, Flex, Box, Stack } from '@chakra-ui/react';
 import { items } from '../../data/items';
 
 const MainGrid: React.FC = () => {
@@ -7,19 +7,14 @@ const MainGrid: React.FC = () => {
     <Flex justifyContent="center">
       <Box w="2xl" h={800} mb={5} mt={5} overflowY="scroll" background="gray.800" borderRadius={5} border="solid 15px" borderColor="gray.800">
         <Flex justifyContent="center">
-          <SimpleGrid columns={5} spacingX="10px" spacingY="10px" w="fit-content">
+          <SimpleGrid columns={5} spacingX="5px" spacingY="5px" w="fit-content">
             {items.map((data) => (
-              <Image
-                src={data.source}
-                alt={data.label}
-                w="100px"
-                h="100px"
-                objectFit="cover"
-                background="gray.700"
-                border="solid 1px"
-                borderColor="gray.600"
-                borderRadius={5}
-              />
+              <Stack w="100px" h="125px" background="gray.700" borderColor="gray.600" borderRadius={5} overflow="hidden" spacing={0}>
+                <Image src={data.source} alt={data.label} w="100px" h="100px" objectFit="contain" />
+                <Box h="25px" color="gray.300" background="gray.600">
+                  {data.label}
+                </Box>
+              </Stack>
             ))}
           </SimpleGrid>
         </Flex>
