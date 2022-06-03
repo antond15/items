9c61c36384c1d87133152a426e62d21ce39599d0
ANTOND <71350868+antond15@users.noreply.github.com>
2022-06-03 17:08:22 +0200

    feat(data/items): More detailed information

--- src/data/items.ts
+++ src/data/items.ts
@@ -2,28 +2,33 @@ import { ItemInterface } from '../typings/item';
 
 export const items: ItemInterface = {
   onion: {
-    label: 'Big slice of onion',
+    label: 'Onion',
     tags: ['vegetable', 'food'],
-    description: 'The onion is a vegetable that is the most widely cultivated species of the genus Allium.',
+    description: 'Onions are usually served cooked, but can also be eaten raw.',
   },
   paperbag: {
     label: 'Paperbag',
     tags: ['food', 'fruit'],
+    description: 'Useful for carrying groceries.',
   },
   'ammo-9': {
     label: 'Ammo 9mm',
     tags: ['ammo', 'weapons'],
+    description: 'Rimless, tapered firearms cartridge.',
   },
   amogus: {
     label: 'Among Us',
-    tags: ['food', 'fruit'],
+    tags: ['toy', 'other'],
+    description: 'Absolutely not an impostor plush from the popular game Among Us ඞ',
   },
   armor: {
     label: 'Armor',
-    tags: ['food', 'fruit'],
+    tags: ['weapons', 'cloth'],
+    description: 'Bulletproof vest that helps absorb the impact and reduce or stop penetration from projectiles.',
   },
   bandage: {
     label: 'Bandage',
-    tags: ['food', 'fruit'],
+    tags: ['medical', 'cloth'],
+    description: 'Woven strip of material with absorbent barrier to prevent adhering to wounds.',
   },
 };
