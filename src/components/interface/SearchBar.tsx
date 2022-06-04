5119f94075a7ad342bcf722042db85cf99abf293
ANTOND <71350868+antond15@users.noreply.github.com>
2022-06-04 23:24:48 +0200

    fix(search/filter): Fix popper css margin warning
    https://github.com/chakra-ui/chakra-ui/issues/3173

--- src/components/interface/SearchBar.tsx
+++ src/components/interface/SearchBar.tsx
@@ -55,38 +55,40 @@ const SearchBar: React.FC<Props> = (props) => {
             borderRadius={5}
             onChange={(e: any) => setQuery(e.target.value.toLowerCase())}
           />
-          <Menu closeOnSelect={false}>
-            <MenuButton as={IconButton} size="sm" aria-label="Filter options" fontSize="md" icon={<FaFilter />} />
-            <MenuList bg="gray.700" borderColor="gray.500" color="gray.100">
-              <MenuOptionGroup type="radio" title="Order" textAlign="left" defaultValue="asc">
-                <MenuItemOption _hover={{ bg: 'gray.600' }} _focus={{ bg: 'gray.600' }} value="asc" onClick={() => setAscending(true)}>
-                  <HStack justify="space-between">
-                    <span>Ascending</span>
-                    <FaSortAlphaDown />
-                  </HStack>
-                </MenuItemOption>
-                <MenuItemOption _hover={{ bg: 'gray.600' }} _focus={{ bg: 'gray.600' }} value="desc" onClick={() => setAscending(false)}>
-                  <HStack justify="space-between">
-                    <span>Descending</span>
-                    <FaSortAlphaUp />
-                  </HStack>
-                </MenuItemOption>
-              </MenuOptionGroup>
-              <MenuDivider />
-              <MenuOptionGroup type="checkbox" title="Tags" textAlign="left" defaultValue={tagDataArr}>
-                {tagDataArr.map((name, key) => {
-                  const tag = tagData[name];
-                  return (
-                    <MenuItemOption key={key} _hover={{ bg: 'gray.600' }} _focus={{ bg: 'gray.600' }} value={name} lineHeight={1}>
-                      <Tag size="sm" bg={tag.color}>
-                        {tag.label}
-                      </Tag>
-                    </MenuItemOption>
-                  );
-                })}
-              </MenuOptionGroup>
-            </MenuList>
-          </Menu>
+          <Box>
+            <Menu closeOnSelect={false}>
+              <MenuButton as={IconButton} size="sm" aria-label="Filter options" fontSize="md" icon={<FaFilter />} />
+              <MenuList bg="gray.700" borderColor="gray.500" color="gray.100">
+                <MenuOptionGroup type="radio" title="Order" textAlign="left" defaultValue="asc">
+                  <MenuItemOption _hover={{ bg: 'gray.600' }} _focus={{ bg: 'gray.600' }} value="asc" onClick={() => setAscending(true)}>
+                    <HStack justify="space-between">
+                      <span>Ascending</span>
+                      <FaSortAlphaDown />
+                    </HStack>
+                  </MenuItemOption>
+                  <MenuItemOption _hover={{ bg: 'gray.600' }} _focus={{ bg: 'gray.600' }} value="desc" onClick={() => setAscending(false)}>
+                    <HStack justify="space-between">
+                      <span>Descending</span>
+                      <FaSortAlphaUp />
+                    </HStack>
+                  </MenuItemOption>
+                </MenuOptionGroup>
+                <MenuDivider />
+                <MenuOptionGroup type="checkbox" title="Tags" textAlign="left" defaultValue={tagDataArr}>
+                  {tagDataArr.map((name, key) => {
+                    const tag = tagData[name];
+                    return (
+                      <MenuItemOption key={key} _hover={{ bg: 'gray.600' }} _focus={{ bg: 'gray.600' }} value={name} lineHeight={1} onClick={() => console.log('cscs')}>
+                        <Tag size="sm" bg={tag.color}>
+                          {tag.label}
+                        </Tag>
+                      </MenuItemOption>
+                    );
+                  })}
+                </MenuOptionGroup>
+              </MenuList>
+            </Menu>
+          </Box>
         </HStack>
       </Box>
     </Flex>
