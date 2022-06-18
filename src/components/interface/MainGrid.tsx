3b4516edae5edabdf9143fcf4e5f59c6df80ef46
ANTOND <71350868+antond15@users.noreply.github.com>
2022-06-18 14:32:30 +0200

    fix(grid): Minimal width

--- src/components/interface/MainGrid.tsx
+++ src/components/interface/MainGrid.tsx
@@ -9,7 +9,7 @@ type Props = {
 const MainGrid: React.FC<Props> = (props) => {
   return (
     <Flex justifyContent="center">
-      <Box h={800} mb={5} mt={2} overflowY="auto" background="gray.800" borderRadius={5} border="solid 15px" borderColor="gray.800">
+      <Box h={800} minW={655} mb={5} mt={2} overflowY="auto" background="gray.800" borderRadius={5} border="solid 15px" borderColor="gray.800">
         <Flex justifyContent="center">
           <SimpleGrid columns={6} spacingX="5px" spacingY="5px" w="fit-content">
             {props.items.map((name, key) => {
