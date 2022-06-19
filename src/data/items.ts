3a5ab8920c3e5a1241f95775a847a3e9fbfd0c81
ANTOND <71350868+antond15@users.noreply.github.com>
2022-06-19 13:27:23 +0200

    chore: Prettier rules for item data

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,34 +1,39 @@
 import { ItemInterface } from '../typings/item';
 
 export const itemData: ItemInterface = {
-  onion: {
-    label: 'Onion',
-    tags: ['vegetable', 'food'],
-    description: 'Onions are usually served cooked, but can also be eaten raw.',
-  },
-  paperbag: {
-    label: 'Paperbag',
-    tags: ['other'],
-    description: 'Useful for carrying groceries.',
-  },
-  'ammo-9': {
-    label: 'Ammo 9mm',
-    tags: ['ammo', 'weapons'],
-    description: 'Rimless, tapered firearms cartridge.',
-  },
-  amogus: {
-    label: 'Among Us',
-    tags: ['toy', 'other'],
-    description: 'Absolutely not an impostor plush from the popular game Among Us ඞ',
-  },
-  armor: {
-    label: 'Armor',
-    tags: ['weapons', 'cloth'],
-    description: 'Bulletproof vest that helps absorb the impact and reduce or stop penetration from projectiles.',
-  },
-  bandage: {
-    label: 'Bandage',
-    tags: ['medical', 'cloth'],
-    description: 'Woven strip of material with absorbent barrier to prevent adhering to wounds.',
-  },
+	'onion': {
+		label: 'Onion',
+		tags: ['vegetable', 'food'],
+		description: 'Onions are usually served cooked, but can also be eaten raw.',
+	},
+
+	'paperbag': {
+		label: 'Paperbag',
+		tags: ['other'],
+		description: 'Useful for carrying groceries.',
+	},
+
+	'ammo-9': {
+		label: 'Ammo 9mm',
+		tags: ['ammo', 'weapons'],
+		description: 'Rimless, tapered firearms cartridge.',
+	},
+
+	'amogus': {
+		label: 'Among Us',
+		tags: ['toy', 'other'],
+		description: 'Absolutely not an impostor plush from the popular game Among Us ඞ',
+	},
+
+	'armor': {
+		label: 'Armor',
+		tags: ['weapons', 'cloth'],
+		description: 'Bulletproof vest that helps absorb the impact and reduce or stop penetration from projectiles.',
+	},
+
+	'bandage': {
+		label: 'Bandage',
+		tags: ['medical', 'cloth'],
+		description: 'Woven strip of material with absorbent barrier to prevent adhering to wounds.',
+	},
 };
