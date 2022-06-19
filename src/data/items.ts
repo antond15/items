7254017c08b85b75bd3d736cdc7c8225d8c700d5
ANTOND <71350868+antond15@users.noreply.github.com>
2022-06-19 20:31:23 +0200

    feat(data): Add ammunition
    Also use VSCode's regions for better readability.

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,6 +1,78 @@
 import { ItemInterface } from '../typings/item';
 
 export const itemData: ItemInterface = {
+	//-----------------------------------------------------------------------------------------------
+	// Ammunition
+	//-----------------------------------------------------------------------------------------------
+	//#region
+
+	'ammo-9': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: '9mm Luger - Most popular cartridge for handguns and SMGs.',
+	},
+
+	'ammo-38': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: '.38 Long Colt - Mostly used in revolvers.',
+	},
+
+	'ammo-44': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: '.44 Magnum - Originaly used in revolvers, later in carbines and rifles.',
+	},
+
+	'ammo-45': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: '.45 ACP - Mostly used in handguns.',
+	},
+
+	'ammo-50': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: '.50 AE - Best known for its usage in Desert Eagle.',
+	},
+
+	'ammo-bb': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: 'Bean bag round - Used for less lethal apprehension of suspects.',
+	},
+
+	'ammo-flare': {
+		label: 'Flare',
+		tags: ['ammo', 'weapons'],
+		description: 'Type of pyrotechnic that produces a bright light without an explosion.',
+	},
+
+	'ammo-musket': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: 'Musket bullet',
+	},
+
+	'ammo-rifle': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: '5.56 NATO - Rifle cartridge for NATO forces.',
+	},
+
+	'ammo-shotgun': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: '12 Gauge - Most popular shotgun shell.',
+	},
+
+	'ammo-sniper': {
+		label: 'Bullet',
+		tags: ['ammo', 'weapons'],
+		description: '7.62mm - Often used in sniper rifles.',
+	},
+	//#endregion
+
 	'onion': {
 		label: 'Onion',
 		tags: ['vegetable', 'food'],
@@ -11,12 +83,6 @@ export const itemData: ItemInterface = {
 		label: 'Paperbag',
 		tags: ['other'],
 		description: 'Useful for carrying groceries.',
-	},
-
-	'ammo-9': {
-		label: 'Ammo 9mm',
-		tags: ['ammo', 'weapons'],
-		description: 'Rimless, tapered firearms cartridge.',
 	},
 
 	'amogus': {
